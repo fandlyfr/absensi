0682653f57cf16cfa23e9c873cb6aaa16ce682fb
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2020-04-17 23:55:22 +0800

    change css priority

--- header.php
+++ header.php
@@ -10,9 +10,9 @@ session_start();
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/header.css">
-	<link rel="stylesheet" type="text/css" href="css/Users.css">
 	<link rel="stylesheet" type="text/css" href="css/userslog.css">
 	<link rel="stylesheet" type="text/css" href="css/manageusers.css">
+	<link rel="stylesheet" type="text/css" href="css/Users.css">
 	<script>
     $(document).ready(function() {
       $.ajax({
