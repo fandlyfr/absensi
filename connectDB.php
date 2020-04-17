6546753503801b573a3b88357d80bed403ccc31c
Muhammad Fandly Fadlurachman <fandlyfr@gmail.com>
2020-04-17 18:46:25 +0800

    update connectDb.php

--- connectDB.php
+++ connectDB.php
@@ -5,6 +5,11 @@
     $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
     $dbname = "biometricattendace";
     
+    // $servername = "localhost";
+    // $username = "fanfanmy_admin";		//put your phpmyadmin username.(default is "root")
+    // $password = "fanfanmy123";			//if your phpmyadmin has a password put it here.(default is "root")
+    // $dbname = "fanfanmy_biometricattendace";
+    
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
 	if ($conn->connect_error) {
